3e938e54b11975555060b579d15b233d814382bf
Jon <jonathon_rylatt@yahoo.com>
2023-12-21 13:11:36 +0800

    Added the class car and got the program to print out the make, model, and year

---
+++ Classes/Car.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Car
+{
+	public string make {  get; set; }
+	public string model { get; set; }
+	public int year { get; set; }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,19 @@
 ﻿namespace Classes
 {
+
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            var newCar = new Car();
+            newCar.make="VW";
+            Console.WriteLine($"{newCar.make}");
+            newCar.model="Golf";
+            Console.WriteLine($"{newCar.model}");
+            newCar.year=2016;
+            Console.WriteLine($"{newCar.year}");
+            Console.ReadLine();
         }
     }
 }
